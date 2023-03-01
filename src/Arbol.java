a3edcb257b06325fe0ef5823ac5c6783872f168e
isLiliAg <aguilar.medina.lilibeth142@gmail.com>
2023-02-28 19:05:29 -0800

    actualizacion de codigo, clase lunes 27 feb

--- src/Arbol.java
+++ src/Arbol.java
@@ -3,36 +3,48 @@ import java.awt.Color;
 public class Arbol {
     //Atributos - Datos tipo double porque pueden ser representados por valores decimales
     //non-static
-    double alturaCm;
-    double diametroTroncoCm;
-    TipoDeArbol tipoArbol;
+    private double alturaCm;
+    private double diametroTroncoCm;
+    private TipoDeArbol tipoArbol;
     //Atributo - static
-    static Color TRONCO_COLOR = new Color(102, 51, 0);
+    protected static Color TRONCO_COLOR = new Color(102, 51, 0);
 
     //Constructor
     //Tendremos que añadir entradas al constructor para poder construir un árbol personalizado
     // con la altura, el diámetro y el tipo.
-    Arbol(double alturaCm, double diametroTroncoCm,
+    public Arbol(double alturaCm, double diametroTroncoCm,
           TipoDeArbol tipoArbol) {
         this.alturaCm = alturaCm;
         this.diametroTroncoCm = diametroTroncoCm;
         this.tipoArbol = tipoArbol;
     }
 
+    public double getAlturaCm(){
+        return alturaCm;
+    }
+
+    public double getDiametroTroncoCm() {
+        return diametroTroncoCm;
+    }
+
+    public TipoDeArbol getTipoArbol(){
+        return tipoArbol;
+    }
+
     //Funcion -> representa comportamiento
-    void crecimiento() {
+    public void crecimiento() {
         this.alturaCm = this.alturaCm + 10;
         this.diametroTroncoCm = this.diametroTroncoCm + 1;
     }
 
-    static void anuncioArbol() {
+    public static void anuncioArbol() {
         System.out.println("Mira el color " +
                 TRONCO_COLOR + "del tronco de ese arbol!");
     }
 
     //Instancia del Objeto -> Arbol
     //Aplicacion logica del comportamiento de un arbol(crecimiento)
-    void anuncioArbolAlto() {
+    public void anuncioArbolAlto() {
         if (this.alturaCm > 100) {
             System.out.println("El arbol de " +
                     this.tipoArbol + " esta muy alto!");
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ import java.awt.Color;
 
 public class Main {
     public static void main(String[] args) {
-        Color troncoColor = Arbol.TRONCO_COLOR;
-        System.out.println(troncoColor);
+
+        Arbol miArbolFavoritoAguacate = new Arbol(180, 30, TipoDeArbol.AGUACATE);
+
+        System.out.println(miArbolFavoritoAguacate.getTipoArbol());
+        System.out.println(miArbolFavoritoAguacate.getAlturaCm());
+        System.out.println(miArbolFavoritoAguacate.getDiametroTroncoCm());
+
+        /*Color troncoColor = Arbol.TRONCO_COLOR;
+        System.out.println(troncoColor);*/
     }
 }
